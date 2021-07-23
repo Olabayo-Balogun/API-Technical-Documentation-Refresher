efa4ca9662004041f990e1dd4503c04652d2bcb0
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-07-23 18:42:44 +0100

    Added data annotation to the "AuthorForUpdate" class as well as XML comments to further improve documentation.

--- Library.API/Models/AuthorForUpdate.cs
+++ Library.API/Models/AuthorForUpdate.cs
@@ -1,9 +1,22 @@
-﻿namespace Library.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.API.Models
 {
     public class AuthorForUpdate
-    {  
+    {
+        /// <summary>
+        ///     The first name of the author
+        /// </summary>
+        //Adding the same data annotation class present in the entity is important for documentation.
+        [Required]
+        [MaxLength(150)]
         public string FirstName { get; set; }
 
+        /// <summary>
+        ///     The last name of the author
+        /// </summary>
+        [Required]
+        [MaxLength(150)]
         public string LastName { get; set; }
     }
 }

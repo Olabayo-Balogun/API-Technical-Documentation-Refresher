e53b7f15906fede78780846cd517da8f71c75c3f
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-07-23 19:41:40 +0100

    Suppressing warnings in classes and in the project properties.

--- Library.API/Entities/Author.cs
+++ Library.API/Entities/Author.cs
@@ -5,7 +5,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Library.API.Entities
 {
-
+    //The line of code can be used to disable warnings relating to missing line of code, note that it can also apply to other warnings once you know the code (which can be found in the error list).
+#pragma warning disable CS1591
     [Table("Authors")]
     public class Author
     {
@@ -22,4 +23,7 @@ namespace Library.API.Entities
 
         public ICollection<Book> Books { get; set; } = new List<Book>();
     }
+
+    //You can restore warnings after a block of code using the line of code below
+#pragma warning restore CS1591
 }
